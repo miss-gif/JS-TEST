c2bbb8c2091f8f008ba7999b452b4ac953a9f64c
miss-gif <ddc.akari@gmail.com>
2022-06-26 22:52:40 +0900

    this

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,24 @@
-function 유저(성, 이름) {
-  this.성name = 성; //  this.성name에 "성"이라는 매개변수 할당
-  this.이름name = 이름; // this.이름name에 "이름"이라는 매개변수 할당
-}
+// 일반(Norml) 함수는 호출 위치에 따라 this 정의!
+// 화살표(Arrow) 함수는 자신의 선언된 함수 범위에서 this 정의
 
-유저.prototype.풀네임 = function () {
-  return `${this.성name} ${this.이름name}`;
+const AKARI = {
+  name: "AKARI",
+  norml: function () {
+    console.log(this.name);
+  },
+  arrow: () => {
+    console.log(this.name);
+  },
 };
 
-const 홍길동 = new 유저("홍", "길동"); // 여기서 new는 생성자 함수
-const 김철수 = new 유저("김", "철수");
-const 김이솔 = new 유저("김", "이솔");
+AKARI.norml(); // AKARI
+AKARI.arrow(); // undefined
 
-console.log(홍길동.풀네임()); // 홍 길동
-console.log(김철수);
-console.log(김이솔);
+const NENE = {
+  name: "Nene",
+  norml: AKARI.norml,
+  arrow: AKARI.arrow,
+};
 
-// 여기서 this는 생성자 함수 new를 통해 생성된 홍길동, 김철수, 김이솔을 지칭함
-// new를 키워드를 통해 생성자 함수로 실행한 결과를 반환해서 할당된 변수를 인스턴스라 부름
-// const 홍길동, const 김철수, const 김이솔 = 인스턴스
+NENE.norml(); // Nene
+NENE.arrow(); // undefined
