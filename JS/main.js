a2317601259d7eab6b5456e2019ada47178426ba
miss-gif <ddc.akari@gmail.com>
2022-06-27 17:48:07 +0900

    구조 분해 할당 1

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,17 @@
+// 구조 분해 할당 (Destructuring assignment) 디스트럭처링
+// 비구조화 할당
+
 const user = {
   name: "akari",
-  ahe: 85,
+  age: 85,
   email: "svx327@naver.com",
 };
 
-const keys = Object.keys(user);
-console.log(keys);
-// ['name', 'age', 'email'] 프로퍼티 이름만 추출해서 배열로 만들어 줌
+const { name, age, email, address } = user; // 필요한 것만 꺼내서 사용 가능
+// E.g user.address
 
-console.log(user["email"]); // ['name', 'ahe', 'email']
-// 객체 데이터에서 사용 가능한 인덱싱 방법
-
-const values = keys.map((key) => user[key]);
-console.log(values); // ['akari', 85, 'svx327@naver.com']
-// 값만 가지고 있는 배열 데이터를 만들 수 있음
+console.log(`사용자의 이름은 ${name}입니다.`); // 사용자의 이름은 akari입니다.
+console.log(`${name}의 나이는 ${age}세입니다.`); // akari의 나이는 85세입니다.
+console.log(`${name}의 이메일 주소는 ${email}입니다.`);
+//akari의 이메일 주소는 svx327@naver.com입니다.
+console.log(address); // undefined
