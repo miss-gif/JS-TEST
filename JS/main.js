3c98a3ede950c0df11cf8c0fa0bf211a31805f35
miss-gif <ddc.akari@gmail.com>
2022-06-27 17:37:47 +0900

    객체 1

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,11 @@
-class Vehicle {
-  constructor(name, wheel) {
-    this.name = name;
-    this.wheel = wheel;
-  }
-}
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
 
-const myVehicle = new Vehicle("운송수단", 2);
-console.log(myVehicle); // 객체 데이터 생성을 확인 할 수 있음
+const returnedTarget = Object.assign(target, source);
+// source 데이터를 target에 덮어쓰기 함 (속성의 이름은 고유해야함)
 
-class Bicycle extends Vehicle {
-  // Vehicle 데이터를 extends 확장(상속)을 통해 사용하겠다.
-  constructor(name, wheel) {
-    super(name, wheel); // 여기서 super는 Vehicle
-  }
-}
+console.log(target);
+// expected output: Object { a: 1, b: 4, c: 5 }
 
-const myBicycle = new Bicycle("삼천리", 2);
-const daughtersBicycle = new Bicycle("세발", 3);
-console.log(myBicycle);
-console.log(daughtersBicycle);
-
-class Car extends Vehicle {
-  constructor(name, wheel, license) {
-    super(name, wheel);
-    this.license = license; // 새로운 로직
-  }
-}
-
-const myCar = new Car("벤츠", 4, true);
-const daughtersCar = new Car("포르쉐", 4, false);
-console.log(myCar);
-console.log(daughtersCar);
+console.log(returnedTarget);
+// expected output: Object { a: 1, b: 4, c: 5 }
