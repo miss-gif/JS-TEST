b772a0f8eaca171fac803688031df8e7d0031723
miss-gif <ddc.akari@gmail.com>
2022-06-27 17:52:32 +0900

    구조 분해 할당 3

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,14 @@
-// 기본값 지정 방법 (자주 사용하진 않으나 필요한 경우가 있음)
+// 객체에서 꺼내오는 속성 이름이 마음에 안들 때, 변수명을 바꾸는 방법
 const user = {
   name: "akari",
   age: 85,
   email: "svx327@naver.com",
 };
 
-const { name, age, email, address = "KR" } = user; // address에 기본값 지정 방법
+const { name: akari, age, email, address } = user;
+// E.g user.address
 
-console.log(address); // KR
+console.log(`사용자의 이름은 ${akari}입니다.`);
+console.log(`${akari}의 나이는 ${age}세입니다.`);
+console.log(`${akari}의 이메일 주소는 ${email}입니다.`);
+console.log(address);
