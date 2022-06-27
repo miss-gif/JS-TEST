f77bb592716245a5c3c2aafb4753aa03a70307e8
miss-gif <ddc.akari@gmail.com>
2022-06-27 18:11:04 +0900

    전개 연산자 2

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,33 @@
-// 전개 연산자 (Spread)
+// 배열 데이터가 추가 된다면
 
-const fruits = ["사과", "바나나", "체리"];
-console.log(fruits); // ['사과', '바나나', '체리'] -- 배열 데이터
-console.log(...fruits); // 사과 바나나 체리 -- 문자 데이터
+const fruits = ["사과", "바나나", "체리", "오렌지"];
 
-function toObj(a, b, c) {
+function toObj(a, b, ...c) {
   return {
     a: a,
     b: b,
     c: c,
+  }; // ...c를 rest parameter(나머지 매개변수)
+}
+
+console.log(toObj(...fruits)); // {a: '사과', b: '바나나', c: ['체리', '오렌지']}
+
+// 변수명과 속성명이 같으면 생략 가능
+
+const fruits = ["사과", "바나나", "체리", "오렌지"];
+
+function toObj(a, b, ...c) {
+  return {
+    a, // 생략
+    b, // 생략
+    c, // 생략
   };
 }
 
 console.log(toObj(...fruits));
-// {a: '사과', b: '바나나', c: '체리'}
-console.log(toObj(fruits[0], fruits[1], fruits[2]));
-// {a: '사과', b: '바나나', c: '체리'}
+
+// 축약형 작성
+
+const fruits = ["사과", "바나나", "체리", "오렌지"];
+const toObj = (a, b, ...c) => ({ a, b, c });
+console.log(toObj(...fruits));
