45aa77c0f2f79456b830fb1e8a0b561cf0f6fbdd
miss-gif <ddc.akari@gmail.com>
2022-06-27 17:40:08 +0900

    객체 2

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,19 @@
-const target = { a: 1, b: 2 };
-const source = { b: 4, c: 5 };
+const userAge = {
+  // key : value형태
+  name: "akari",
+  ahe: 85,
+};
+const userEmail = {
+  name: "akari",
+  email: "svx327@naver.com",
+};
 
-const returnedTarget = Object.assign(target, source);
-// source 데이터를 target에 덮어쓰기 함 (속성의 이름은 고유해야함)
-
+const target = Object.assign(userAge, userEmail); // 대상 객체, 출처 객체
 console.log(target);
-// expected output: Object { a: 1, b: 4, c: 5 }
+console.log(userAge);
+console.log(target === userAge); // true 같은 메모리 주소를 보고 있음.
 
-console.log(returnedTarget);
-// expected output: Object { a: 1, b: 4, c: 5 }
+const a = { k: 123 };
+const b = { k: 123 };
+console.log(a === b); // false
+// 다른 메모리 주소를 보고 있음. 참조형 데이터(객체, 배열, 함수)의 특징
