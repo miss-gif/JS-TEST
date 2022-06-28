d697ea9175021653f4224226d5f2bb27d6e536a9
miss-gif <ddc.akari@gmail.com>
2022-06-28 19:59:04 +0900

    데이터 불변성

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,8 @@
-// 배열 데이터가 추가 된다면
+// 데이터 불변성(Immutability)
+// 원시 데이터: String, Number, Boolean, undefined, null
+// 참조형 데이터: Object, Array, Function
 
-const fruits = ["사과", "바나나", "체리", "오렌지"];
+// 원시 데이터 = 값이 같으면 같은 데이터, 다르면 다른 데이터
+// 참조형 데이터 = 값이 같아도 다른 데이터 일 수 있다
 
-function toObj(a, b, ...c) {
-  return {
-    a: a,
-    b: b,
-    c: c,
-  }; // ...c를 rest parameter(나머지 매개변수)
-}
-
-console.log(toObj(...fruits)); // {a: '사과', b: '바나나', c: ['체리', '오렌지']}
-
-// 변수명과 속성명이 같으면 생략 가능
-
-const fruits = ["사과", "바나나", "체리", "오렌지"];
-
-function toObj(a, b, ...c) {
-  return {
-    a, // 생략
-    b, // 생략
-    c, // 생략
-  };
-}
-
-console.log(toObj(...fruits));
-
-// 축약형 작성
-
-const fruits = ["사과", "바나나", "체리", "오렌지"];
-const toObj = (a, b, ...c) => ({ a, b, c });
-console.log(toObj(...fruits));
+// 참조형 데이터를 구분해서 관리하고 싶다면 복사 개념을 사용해야 함
