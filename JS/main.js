e7a4cd944f414828a66e487a2b21025ce82fdabf
miss-gif <ddc.akari@gmail.com>
2022-06-29 22:27:50 +0900

    얕은 복사(Shallow copy)와 깊은 복사(Deep copy)

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,47 @@
-// 데이터 불변성(Immutability)
-// 원시 데이터: String, Number, Boolean, undefined, null
-// 참조형 데이터: Object, Array, Function
+// Object.assign()으로 얕은 복사
+const user = {
+  name: "Akari",
+  age: 85,
+  emails: ["svx327@naver.com"],
+};
+const copyUser = Object.assign({}, user);
+console.log(copyUser === user); // false
 
-// 원시 데이터 = 값이 같으면 같은 데이터, 다르면 다른 데이터
-// 참조형 데이터 = 값이 같아도 다른 데이터 일 수 있다
+user.age = 22;
 
-// 참조형 데이터를 구분해서 관리하고 싶다면 복사 개념을 사용해야 함
+console.log("user", user); // {name: 'Akari', age: 22, emails: Array(1)}
+console.log("copyUser", copyUser); // {name: 'Akari', age: 85, emails: Array(1)}
+
+// 전개 연산자로 얕은 복사
+const user = {
+  name: "Akari",
+  age: 85,
+  emails: ["svx327@naver.com"],
+};
+const copyUser = { ...user };
+console.log(copyUser === user); // false
+
+user.age = 22;
+
+console.log("user", user); // {name: 'Akari', age: 22, emails: Array(1)}
+console.log("copyUser", copyUser); // {name: 'Akari', age: 85, emails: Array(1)}
+
+// 얕은 복사의 한계
+const user = {
+  name: "Akari",
+  age: 85,
+  emails: ["svx327@naver.com"],
+};
+const copyUser = { ...user };
+console.log(copyUser === user); // false
+
+user.age = 22;
+
+console.log("user", user); // {name: 'Akari', age: 22, emails: Array(1)}
+console.log("copyUser", copyUser); // {name: 'Akari', age: 85, emails: Array(1)}
+
+user.emails.push("svx327@nate.com");
+// emails는 배열 데이터(참조형 데이터), 같은 주소를 공유하고 있음
+console.log(user.emails === copyUser.emails); // true
+console.log("user", user); // {name: 'Akari', age: 22, emails: Array(2)}
+console.log("copyUser", copyUser); // {name: 'Akari', age: 85, emails: Array(2)}
