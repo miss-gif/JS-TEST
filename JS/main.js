c5a2668f099a7ed2487a536cb74477304e091257
miss-gif <ddc.akari@gmail.com>
2022-07-01 01:39:08 +0900

    가져오기, 내보내기

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,9 @@
-import _ from "lodash"; // lodash 패키지 사용
+import _ from "lodash"; // From `node_modules`!
+import checkType from "./getType"; // getType.js
+// import { random, user as akari } from "./getRandom"; // getRandom/js
+import * as R from "./getRandom";
 
-const user = {
-  name: "Akari",
-  age: 85,
-  emails: ["svx327@naver.com"],
-};
-const copyUser = _.cloneDeep(user); // cloneDeep라는 메소드를 사용
-console.log(copyUser === user); // false
-
-user.age = 22;
-
-console.log("user", user); // {name: 'Akari', age: 22, emails: Array(1)}
-console.log("copyUser", copyUser); // {name: 'Akari', age: 85, emails: Array(1)}
-
-console.log("------");
-console.log("------");
-
-user.emails.push("svx327@nate.com");
-console.log(user.emails === copyUser.emails); // false
-console.log("user", user); // {name: 'Akari', age: 22, emails: Array(2)}
-console.log("copyUser", copyUser); // {name: 'Akari', age: 85, emails: Array(1)}
+console.log(_.camelCase("the hello world"));
+console.log(checkType([1, 2, 3]));
+// console.log(random(), random());
+console.log(R);
